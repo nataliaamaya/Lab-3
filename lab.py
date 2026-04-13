e683a5742c273ee984625bac434eb0b3cba5bca7
nataliaamaya <natalia.amaya@generalassemb.ly>
2026-04-13 13:13:21 -0400

    Lab-3-Completed

--- lab.py
+++
@@ -1,8 +0,0 @@
-records = [
-    {"name": "Ava", "score": 95, "hours_studied": 10},
-    {"name": "Ben", "score": None, "hours_studied": 8},
-    {"name": "Cara", "score": 88, "hours_studied": None},
-    {"name": "", "score": 72, "hours_studied": 6},
-    {"name": "Eli", "score": 105, "hours_studied": 9},
-    {"name": "Finn", "score": 65, "hours_studied": 7}
-]
